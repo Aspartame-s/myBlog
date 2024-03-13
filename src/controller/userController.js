7b57025033c3a094ceb5b21dc39a4d9890b7a611
Aspartame-s <jiahuicheng639@gmail.com>
2024-03-13 20:37:22 +0800

    userapi login接口

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,8 +1,14 @@
+const {exec} = require('../db/mysql')
 const loginCheck = (username, password) => {
-    if(username == 'zhangsan' && password == '123456') {
-        return true
-    }
-    return false
+    // if(username == 'zhangsan' && password == '123456') {
+    //     return true
+    // }
+    // return false
+    let sql = `select username, realname from users where username='${username}' and password='${password}'`
+    return exec(sql).then(rows => {
+        console.log(rows)
+        return rows[0] || {}
+    })
 }
 
 module.exports = {
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,15 +1,21 @@
-const {loginCheck} = require('../controller/userController')
+const { loginCheck } = require('../controller/userController')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleUserRouter = (req, res) => {
-    if(req.method == 'POST' && req.path == '/api/user/login') {
-        const {username, password} = req.body
+    if (req.method == 'POST' && req.path == '/api/user/login') {
+        const { username, password } = req.body
         const result = loginCheck(username, password)
-        if(result) {
-            return new SuccessModel()
-        }else {
+        return result.then(data => {
+            if (data.username) {
+                return new SuccessModel()
+            }
             return new ErrorModel('登录失败，请检查用户名和密码')
-        }
+        })
+        // if (result) {
+        //     return new SuccessModel()
+        // } else {
+        //     return new ErrorModel('登录失败，请检查用户名和密码')
+        // }
     }
 }
 
