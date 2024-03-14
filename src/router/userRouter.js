676d877a888a54bfc64ad5999053042a0620711b
Aspartame-s <jiahuicheng639@gmail.com>
2024-03-14 14:55:45 +0800

    新增cookie限制

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,6 +1,12 @@
 const { login } = require('../controller/userController')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
+
 const handleUserRouter = (req, res) => {
     if (req.method == 'GET' && req.path == '/api/user/login') {
         // const { username, password } = req.body
@@ -8,7 +14,7 @@ const handleUserRouter = (req, res) => {
         const result = login(username, password)
         return result.then(data => {
             if (data.username) {
-                res.setHeader('Set-Cookie', `username=${username}; path=/; httpOnly`)
+                res.setHeader('Set-Cookie', `username=${username}; path=/; httpOnly; expires=${getCookieExpires()}`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败，请检查用户名和密码')
