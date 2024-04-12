5048a456b5073fe7b5a7f7fa7f0870e440d67ea6
Aspartame-s <jiahuicheng639@gmail.com>
2024-04-12 17:17:23 +0800

    密码加密

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,10 +1,12 @@
 const {exec, escape} = require('../db/mysql')
+const {genPassword} = require('../utils/crypto')
 const login = (username, password) => {
     // if(username == 'zhangsan' && password == '123456') {
     //     return true
     // }
     // return false
     username = escape(username)
+    password = genPassword(password)
     password = escape(password)
     let sql = `select username, realname from users where username=${username} and password=${password}`
     console.log(sql)
